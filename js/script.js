abe73856c859814bd39a4befb409776168234ffd
jennifer_b <jennifer.brunker@gmail.com>
2021-04-15 19:18:04 -0400

    Added js to select and display images from api

--- js/script.js
+++ js/script.js
@@ -6,7 +6,23 @@ const img = document.querySelector(".img");
 async function getImage(){
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+function selectRandomImage(images){
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+}
+
+function displayImage(randomImage){
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function(){
+    getImage();
+});
